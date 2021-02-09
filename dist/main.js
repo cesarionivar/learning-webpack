7bc2de8e4d86e44e135d56dec2ca4d94fcf095e4
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-02-09 11:54:14 -0400

    Adding webpack.config.js Transpilando codigo con babel

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{var o;console.log("Hola mundo sin configuración con webpack 😱"),(o=console).log.apply(o,[1,2,3])})();
---
+++ src/index.js
@@ -0,0 +1,7 @@
+const arr = [1, 2, 3];
+
+const codeESNext = () => console.log(...arr);
+
+console.log('Hola mundo sin configuración con webpack 😱');
+
+codeESNext();
